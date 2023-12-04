5317f8dc0b939a773e7095bfc85bb65ab1d7cff7
Tim Richards <richards@cs.umass.edu>
2023-12-04 19:01:24 +0000

    Fix compilation errors

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,8 +1,13 @@
+#!/usr/bin/env node
+
 import Tap from '../lib/Tap';
 import Points from '../lib/Points';
 
-const results: { passing: any[]; failing: any[] } =
-  await Tap.readTapFromStdIn();
-const resultsWithPoints = Points.calculatePoints(results);
+const main = async () => {
+  const results: { passing: any[]; failing: any[] } =
+    await Tap.readTapFromStdIn();
+  const resultsWithPoints = Points.calculatePoints(results);
+};
 
+const resultsWithPoints = main();
 console.log(JSON.stringify(resultsWithPoints, null, 2));
