6ebe20e08c8f2b5a9f3b8db6b7ee83d72a9e4886
Tim Richards <richards@cs.umass.edu>
2023-12-11 14:47:56 +0000

    Add commander

--- src/bin/ada.ts
+++ src/bin/ada.ts
@@ -1,8 +1,42 @@
 #!/usr/bin/env node
 
-import { TestRunnerFactory, TestResults } from '../lib/autograder/test-runner.js';
+import { Command } from 'commander';
 
-async function main() {
+import {
+  TestRunnerFactory,
+  TestResults,
+} from '../lib/autograder/test-runner.js';
+
+const program = new Command();
+
+program
+  .name('ada')
+  .description('Ada Autograder')
+  .version('0.1.0');
+
+program
+  .command('autograde')
+  .description('Invokes the autograder')
+  .action(async () => {
+    await ada_autograde();
+  });
+
+program
+  .command('subcommand2 <requiredArg>')
+  .description('Run subcommand 2 with a required argument')
+  .option('-o, --optional [value]', 'An optional argument')
+  .action((requiredArg, options) => {
+    console.log(`Subcommand 2 executed with required argument: ${requiredArg}`);
+    if (options.optional) {
+      console.log(`Optional argument: ${options.optional}`);
+    }
+  });
+
+program.parse(process.argv);
+
+// Subcommands Below //
+
+async function ada_autograde() {
   try {
     const runner = TestRunnerFactory.makeNodeTestRunner(['./tests']);
     const results: TestResults = await runner.runTests();
@@ -12,7 +46,3 @@ async function main() {
     console.log(error.message);
   }
 }
-
-main().then(() => {
-  console.log('Done.');
-});
